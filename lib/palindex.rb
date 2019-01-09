671303cd11b8ab7b3eb25c831b06c9fc1e61be4e
Rajan Bhattarai <cdrrazan@gmail.com>
2019-01-09 15:49:51 +0545

    Palindex Gem finalized and pushed to the RubyGems for the publication

--- lib/palindex.rb
+++ lib/palindex.rb
@@ -1,6 +1,19 @@
 require "palindex/version"
 
-module Palindex
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  def letters
+    self.chars.select { |c| c.match(/a-zA-Z/i)}.join
+  end
+
+  private
+
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
+
 end
